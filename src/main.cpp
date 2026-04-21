2957b99a1821f1263ff3e4937bbe05734a723785
Hongan <15937823@qq.com>
2026-04-21 20:32:27 +0800

    feat: enforce single instance per directory, activate existing window on relaunch

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,67 @@
 #include <wx/wx.h>
+#include <wx/snglinst.h>
 #include "ui/MainFrame.h"
 #include "core/ConfigManager.h"
 #include "utils/PathUtils.h"
 
+#ifdef __WXMSW__
+#include <windows.h>
+#endif
+
 class MTCApp : public wxApp {
 public:
     bool OnInit() override {
-        // 设置程序语言环境
         wxLocale* locale = new wxLocale();
         locale->Init(wxLANGUAGE_DEFAULT);
-        
-        // 获取程序所在目录
+
         auto appDir = PathUtils::GetExecutableDir();
-        
-        // 初始化配置管理器
+
+        // 基于程序目录的单实例检测，不同目录的程序各自独立
+        wxString dirId = wxString(appDir).Lower();
+        dirId.Replace("\\", "_");
+        dirId.Replace("/", "_");
+        dirId.Replace(":", "_");
+        dirId.Replace(" ", "_");
+
+        m_instanceChecker = new wxSingleInstanceChecker("MTC-" + dirId);
+
+        if (m_instanceChecker->IsAnotherRunning()) {
+            ActivateExistingWindow();
+            delete m_instanceChecker;
+            return false;
+        }
+
         if (!ConfigManager::GetInstance().Initialize(appDir)) {
-            wxMessageBox(wxT("无法初始化配置，程序将使用默认设置"), 
+            wxMessageBox(wxT("无法初始化配置，程序将使用默认设置"),
                          wxT("警告"), wxOK | wxICON_WARNING);
         }
-        
-        // 创建主窗口
+
         MainFrame* frame = new MainFrame();
         frame->Show(true);
-        
+
         return true;
     }
-    
+
     int OnExit() override {
-        // 保存配置
+        delete m_instanceChecker;
         ConfigManager::GetInstance().SaveConfig();
         return wxApp::OnExit();
+    }
+
+private:
+    wxSingleInstanceChecker* m_instanceChecker = nullptr;
+
+    void ActivateExistingWindow() {
+#ifdef __WXMSW__
+        HWND hwnd = ::FindWindow(NULL, wxString(wxT("MTC - 终端环境管理器")).wc_str());
+        if (hwnd) {
+            if (::IsIconic(hwnd)) {
+                ::ShowWindow(hwnd, SW_RESTORE);
+            }
+            ::SetForegroundWindow(hwnd);
+            ::BringWindowToTop(hwnd);
+        }
+#endif
     }
 };
 
